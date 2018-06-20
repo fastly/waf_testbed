8bd14a9df128b16c7780b8979a374c17fa52f13a
Christian Peron <cperon@fastly.com>
2018-06-20 16:40:34 -0500

    Update python-poise dep and use git
    - Pin chef version
    - Update the version of python-poise
    - Switch to the git version of FTW by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@
 #  DetectionOnly: Passively monitor, log but do NOT impact the HTTP transaction (default)
 #
 default['waf_testbed']['engine_mode'] = 'DetectionOnly'
-default['waf_testbed']['ftw']['use_git'] = false
+default['waf_testbed']['ftw']['use_git'] = true
 default['waf_testbed']['ftw']['branch'] = 'master'
 default['waf_testbed']['ftw']['pip_version'] = '1.0.1'
